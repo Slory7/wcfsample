5fc3dcfd3ff2153552b805d76f609c67205d2f2c
tianpenghui <tianpenghui@JT094502>
2017-08-09 10:55:00 +0800

    add file

---
+++ Business.Manager/Order/BizFlowSteps/BMSteps/A3_StepSaveOrder.cs
@@ -0,0 +1,76 @@
+﻿using Business.Core.Interfaces;
+using Service.Contracts.ViewModels.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Contracts;
+using Business.Manager.Order.Internal;
+using Microsoft.Practices.Unity;
+using Business.Manager.Interfaces;
+using Business.Manager.Order.Interfaces;
+
+namespace Business.Manager.Order.BizFlowSteps.BMSteps
+{
+    /// <summary>
+    /// Save Order
+    /// </summary>
+    public class A3_StepSaveOrder : IBizFlowStep<OrderBiz>
+    {
+        private IManagerCommon _managerCommon;
+        private IOrderBatchManager _bachManager;
+
+        public A3_StepSaveOrder(IManagerCommon managerCommon
+            , IOrderBatchManager bachManager)
+        {
+            this._managerCommon = managerCommon;
+            this._bachManager = bachManager;
+        }
+
+        public string BizType
+        {
+            get
+            {
+                return BizTypeEnum.BM.ToString();
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool AllowParallel { get { return false; } }
+
+        public int NextStep
+        {
+            get
+            {
+                return 0;//0:auto, -1:end, number:step
+            }
+        }
+
+        public int StepNumber
+        {
+            get
+            {
+                return 10;
+            }
+        }
+
+        public ResultData<object> ProcessStep(OrderBiz source, object prevResult)
+        {
+            var result = new ResultData<object>();
+            _managerCommon.DoSomeThing();
+            //TODO: do some thing
+            var resultObject = new ProcessObject();
+            resultObject.NewOrderCode = Guid.NewGuid().ToString();
+            result.Data = resultObject;
+            return result; 
+        }
+    }
+}
